3d8bfd18200fcdaa7d5e175bf238c5b33a60f38c
ianK-code <kian99564@gmail.com>
2023-07-20 00:35:29 +0300

    Creating register page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,5 +1,63 @@
-function Register() {
-  return <div>Register</div>;
-}
+import "../output.css";
 
-export default Register;
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from "@mantine/core";
+
+export default function Register() {
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?{" "}
+        <Anchor size="sm" component="button">
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="lg">
+          {/* <Checkbox label="Remember me" /> */}
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Create
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
