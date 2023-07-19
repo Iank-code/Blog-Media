ff044e5e5d4bb5d3f2614f46e76e7daf2141b49a
ianK-code <kian99564@gmail.com>
2023-07-20 00:14:02 +0300

    Adding mantine notification

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,27 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <Notifications />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
