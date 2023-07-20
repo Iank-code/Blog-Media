ba02fdfff96acf5e6b40d11c2cb7aca20c54ae91
ianK-code <kian99564@gmail.com>
2023-07-20 23:27:54 +0300

    User model done

---
+++ server/routes/Auth.route.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+// Register routes
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+    zipcode: req.body.zipcode,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
